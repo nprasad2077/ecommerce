e81cbbd3b87550741821dff972a92858e955f13d
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-15 15:55:06 -0600

    font awesome

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,12 +11,12 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">ProShop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="mr-auto">
+            <Nav.Link href="/cart">Cart</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
