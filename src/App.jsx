b53c67e3c25659a2f4f1e1bae95c451136caed83
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-15 15:29:36 -0600

    header + footer create

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+// Components
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      App
+      <Header />
+      <Footer />
+
+    </div>
   )
 }
 
