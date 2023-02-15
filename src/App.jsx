73a91697ae4d0af388eeb3fa64ceae60dc27f62d
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-15 16:51:09 -0600

    dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap'
 // Components
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import HomeScreen from './screens/HomeScreen/HomeScreen'
 
 const App = () => {
   return (
