b7926a036fda4690dd017ce190b004184099c5a2
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-16 16:44:21 -0600

    product name display vite

--- src/screens/ProductScreen/ProductScreen.jsx
+++ src/screens/ProductScreen/ProductScreen.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+
+
+// Components 
+import Rating from '../../components/Rating/Rating'
+import products from '../../products'
 
 const ProductScreen = () => {
+  const {id} = useParams()
+
+  console.log(id);
+
+  const product = products.find((p) => p._id == id)
+
+
+
+
   return (
-    <div>ProductScreen</div>
+    <div>
+      {product.name}
+    </div>
   )
 }
 
