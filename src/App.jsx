c73ea0c8c4117a17dc705ae3e689405e499eac30
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-23 21:44:52 -0600

    cart reducer ready

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import HomeScreen from './screens/HomeScreen/HomeScreen'
 import ProductScreen from './screens/ProductScreen/ProductScreen'
+import CartScreen from './screens/CartScreen/CartScreen'
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
             <Routes>
                 <Route exact path="/" element={<HomeScreen />}  />
                 <Route path="/product/:id" element={<ProductScreen />} />
+                <Route path="/cart/:id?" element={<CartScreen />} />
               </Routes>
           </Container>
         </main>
