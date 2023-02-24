2905b3cbb04ac0a540ab30acd53e536133595a94
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-23 19:43:01 -0600

    cart function ready!

--- src/screens/ProductScreen/ProductScreen.jsx
+++ src/screens/ProductScreen/ProductScreen.jsx
@@ -9,6 +9,8 @@ import {listProductDetails} from '../../actions/productActions'
 // Components 
 import Rating from '../../components/Rating/Rating'
 import products from '../../products'
+import Loader from '../../components/Loader/Loader'
+import Message from '../../components/Message/Message'
 
 const ProductScreen = () => {
 
@@ -21,14 +23,82 @@ const ProductScreen = () => {
 
   useEffect(() => {
     dispatch(listProductDetails(id))
-  }, [])
+  }, [dispatch, id])
 
 
 
   return (
     <div>
       <Link to='/' className='btn btn-light my-3'>Go Back</Link>
-      <Row>
+      {loading ?
+        <Loader />
+        : error
+          ? <Message variant='danger'>{error}</Message>
+        : (      <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid/>
+          </Col>
+  
+          <Col md={3}>
+            <ListGroup variant='flush'>
+  
+              <ListGroupItem>
+                <h3>{product.name}</h3>
+              </ListGroupItem>
+  
+              <ListGroupItem>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+              </ListGroupItem>
+  
+              <ListGroupItem>
+                Price: {product.price}
+              </ListGroupItem>
+  
+              <ListGroupItem>
+                Description: {product.description}
+              </ListGroupItem>
+  
+            </ListGroup>
+          </Col>
+  
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroupItem>
+                  <Row>
+                    <Col>
+                      Price: 
+                    </Col>
+                    <Col>
+                      <strong>{product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+  
+                <ListGroupItem>
+                  <Row>
+                    <Col>
+                      Status: 
+                    </Col>
+                    <Col>
+                      {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+  
+                <ListGroupItem>
+                  <Button className='btn-block' type='button' disabled={product.countInStock == 0}>Add to Cart</Button>
+                </ListGroupItem>
+              </ListGroup>
+            </Card>
+  
+          </Col>
+        </Row>)
+
+      }
+
+
+      {/* <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid/>
         </Col>
@@ -87,7 +157,7 @@ const ProductScreen = () => {
           </Card>
 
         </Col>
-      </Row>
+      </Row> */}
     </div>
   )
 }
