b3a4be9246406fb1401480c893e3106e68ed53d7
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-23 19:31:28 -0600

    details load in console

--- src/screens/ProductScreen/ProductScreen.jsx
+++ src/screens/ProductScreen/ProductScreen.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import {listProductDetails} from '../../actions/productActions'
 
 
 // Components 
@@ -9,11 +11,17 @@ import Rating from '../../components/Rating/Rating'
 import products from '../../products'
 
 const ProductScreen = () => {
-  const {id} = useParams()
 
+  const {id} = useParams()
   console.log(id);
 
-  const product = products.find((p) => p._id == id)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(listProductDetails(id))
+  }, [])
+
+  let product = {}
 
 
 
