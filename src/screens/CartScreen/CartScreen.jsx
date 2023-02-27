42ebeace5678651f571c43f54770573e5a794f03
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-27 17:47:35 -0600

    import uselocation

--- src/screens/CartScreen/CartScreen.jsx
+++ src/screens/CartScreen/CartScreen.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
+import {addToCart} from '../../actions/cartActions'
+import { useParams } from 'react-router-dom'
+import Message from '../../components/Message/Message'
 
-const CartScreen = () => {
+
+
+const CartScreen = ({match, history}) => {
+  const {id} = useParams()
+  const location = useLocation()
+  const productID = id
+  const qty = location.search
+
+  console.log(qty);
+
+
   return (
     <div>Cart</div>
   )
