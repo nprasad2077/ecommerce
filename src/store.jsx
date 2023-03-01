575960dd7e7ca61e018f48c5f260764d90e7f085
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-01 17:02:12 -0600

    user details create

--- src/store.jsx
+++ src/store.jsx
@@ -4,7 +4,7 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer, productDetailReducer } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
+import { userLoginReducer, userRegisterReducer, userDetailsReducer} from "./reducers/userReducers";
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -12,6 +12,7 @@ const reducer = combineReducers({
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
 })
 
 const cartItemFromStorage = localStorage.getItem('cartItems') ? 
