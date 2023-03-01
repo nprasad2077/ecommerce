a0cf598ab80fc8fe460ad04d723fc92e4e68a3e8
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-01 17:26:31 -0600

    details loaded on profile update

--- src/screens/ProfileScreen/ProfileScreen.jsx
+++ src/screens/ProfileScreen/ProfileScreen.jsx
@@ -10,10 +10,79 @@ import { getUserDetails } from '../../actions/userActions'
 
 
 const ProfileScreen = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState('')
+    const dispatch = useDispatch()
+    const location = useLocation()
+    const navigation = useNavigate()
+    
+    const userDetails = useSelector(state => state.userDetails)
+    const {error, loading, user} = userDetails
+
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+
+    useEffect(() => {
+        if (!userInfo) {
+            navigation('/login')
+        } else {
+            if(!userInfo || !user.name) {
+                dispatch(getUserDetails('profile'))
+            } else {
+                setName(user.name)
+                setEmail(user.email)
+            }
+        }
+    }, [navigation, userInfo, dispatch, user])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if (password != confirmPassword) {
+            setMessage('Passwords do not match!')
+        } else {
+            console.log('updating');
+        } 
+    }
+
+
   return (
     <Row>
         <Col md={3}>
             <h2>User Profile</h2>
+            {message && <Message variant='danger'>{message}</Message>}
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader></Loader>}
+
+            <Form onSubmit={submitHandler}>
+                <FormGroup controlId='name'>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl required type='name' placeholder='Enter Name' value={name} onChange={(e) => setName(e.target.value)}>
+                    </FormControl>
+                </FormGroup>
+
+                <FormGroup controlId='email'>
+                    <FormLabel>Email Address</FormLabel>
+                    <FormControl required type='email' placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)}>
+                    </FormControl>
+                </FormGroup>
+
+                <FormGroup controlId='password'>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl type='password' placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)}>
+                    </FormControl>
+                </FormGroup>
+
+                <FormGroup controlId='passwordConfirm'>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl type='password' placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}>
+                    </FormControl>
+                </FormGroup>
+
+                <Button type='submit' variant='primary'>Update</Button>
+            </Form>
         </Col>
         <Col md={9}>
             <h2>My Orders</h2>
