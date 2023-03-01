7f2b27ecc6b8afa4765cd2d25f024a7fd9d4776c
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-01 12:47:16 -0600

    container styling and center

--- src/screens/LoginScreen/LoginScreen.jsx
+++ src/screens/LoginScreen/LoginScreen.jsx
@@ -4,15 +4,18 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../../components/Loader/Loader'
 import Message from '../../components/Message/Message'
+import FormContainer from '../../components/FormContainer/FormContainer'
 import { login } from '../../actions/userActions'
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    
+
   return (
-    <div>LoginScreen</div>
+    <FormContainer>
+        Login Screen
+    </FormContainer>
   )
 }
 
