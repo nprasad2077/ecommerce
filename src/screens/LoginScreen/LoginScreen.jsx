33153df7c17f1c92ee8d12946cc4248759b968c2
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-01 12:54:11 -0600

    login page framework

--- src/screens/LoginScreen/LoginScreen.jsx
+++ src/screens/LoginScreen/LoginScreen.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import { Form, Button, Row, Col } from 'react-bootstrap'
+import { Form, Button, Row, Col, FormGroup, FormLabel, FormControl } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../../components/Loader/Loader'
 import Message from '../../components/Message/Message'
@@ -11,10 +11,36 @@ const LoginScreen = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log('submit');
+    }
+
 
   return (
     <FormContainer>
-        Login Screen
+        <h1>Sign In</h1>
+
+        <Form onSubmit={submitHandler}>
+            <FormGroup controlId='email'>
+                <FormLabel>Email Address</FormLabel>
+                <FormControl type='email' placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)}>
+
+                </FormControl>
+
+            </FormGroup>
+
+            <FormGroup controlId='password'>
+                <FormLabel>Password</FormLabel>
+                <FormControl type='password' placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)}>
+
+                </FormControl>
+
+            </FormGroup>
+
+            <Button type='submit' variant='primary'>Sign In</Button>
+
+        </Form>
     </FormContainer>
   )
 }
