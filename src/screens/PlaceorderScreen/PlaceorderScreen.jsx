8bc0ca00c64b967eb64023d797b20586a45aa5ca
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-02 09:05:03 -0600

    payment method

--- src/screens/PlaceorderScreen/PlaceorderScreen.jsx
+++ src/screens/PlaceorderScreen/PlaceorderScreen.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { Button, FormGroup, FormLabel, FormControl, Row, Col, Image, Card } from 'react-bootstrap'
+import { Button, FormGroup, FormLabel, FormControl, Row, Col, Image, Card, ListGroup, ListGroupItem } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import CheckoutSteps from '../../components/CheckoutSteps/CheckoutSteps'
 import { useLocation, useNavigate } from 'react-router-dom'
@@ -14,6 +14,33 @@ const PlaceorderScreen = () => {
   return (
     <div>
         <CheckoutSteps step1 step2 step3 step4/>
+        <Row>
+            <Col md={8}>
+                <ListGroup variant='flush'>
+                    <ListGroupItem>
+                        <h2>Shipping</h2>
+                        <p>
+                            <strong>Shipping: </strong>
+                            {cart.shippingAddress.address}, {cart.shippingAddress.city},
+                            {'  '}
+                            {cart.shippingAddress.postalCode}, 
+                            {'  '}
+                            {cart.shippingAddress.country}
+                        </p>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <h2>Payment Method</h2>
+                        <p>
+                            <strong>Method: </strong>
+                            {cart.paymentMethod}
+                        </p>
+                    </ListGroupItem>
+                </ListGroup>
+            </Col>
+
+            <Col md={4}>
+            </Col>
+        </Row>
     </div>
   )
 }
