8994e3c6dfe9bb114dc3e1f5c7eeb8bca31b7b2b
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-13 16:59:02 -0500

    create and add constants

--- src/reducers/cartReducers.jsx
+++ src/reducers/cartReducers.jsx
@@ -50,7 +50,6 @@ export const cartReducer = (state = {cartItems: [], shippingAddress: {} }, actio
                 cartItems: []
             }
 
-
         default:
             return state
     }
