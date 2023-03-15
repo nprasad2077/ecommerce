94a61f2394a01a8b3fb2d2132c70600bc921b5df
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-15 16:55:25 -0500

    redirect if !admin works!

--- src/screens/UserListScreen/UserListScreen.jsx
+++ src/screens/UserListScreen/UserListScreen.jsx
@@ -9,13 +9,22 @@ import { listUsers } from '../../actions/userActions'
 
 const UserListScreen = () => {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const userList = useSelector(state => state.userList)
     const {loading, error, users} = userList
 
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+
     useEffect(()=> {
-        dispatch(listUsers())
-    }, [dispatch])
+        if (userInfo && userInfo.isAdmin){
+            dispatch(listUsers())
+        } else {
+            navigate('/login')
+        }
+        
+    }, [dispatch, navigate])
 
     const deleteHandler = (id) => {
         console.log(id)
