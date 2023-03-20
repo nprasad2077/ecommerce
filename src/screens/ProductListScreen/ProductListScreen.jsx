586501f46dd36c89513415475cd7251c011de6c6
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-20 16:40:56 -0500

    delete works!;

--- src/screens/ProductListScreen/ProductListScreen.jsx
+++ src/screens/ProductListScreen/ProductListScreen.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../../components/Loader/Loader'
 import Message from '../../components/Message/Message'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
-import { listProducts } from '../../actions/productActions'
+import { listProducts, deleteProduct } from '../../actions/productActions'
 
 const ProductListScreen = () => {
     const dispatch = useDispatch()
@@ -13,6 +13,9 @@ const ProductListScreen = () => {
 
     const productList = useSelector(state => state.productList)
     const {loading, error, products} = productList
+
+    const productDelete = useSelector(state => state.productDelete)
+    const {loading: loadingDelete, error: errorDelete, success: successDelete} = productDelete
 
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
@@ -24,11 +27,11 @@ const ProductListScreen = () => {
             navigate('/login')
         }
         
-    }, [dispatch, navigate, userInfo])
+    }, [dispatch, navigate, userInfo, successDelete])
 
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure you want to delete this product?')) {
-            //delete products
+            dispatch(deleteProduct(id))
         }
     }
 
@@ -48,6 +51,10 @@ const ProductListScreen = () => {
                 </Button>
             </Col>
         </Row>
+
+        {loadingDelete && <Loader />}
+        {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+
         {loading
         ? <Loader />
         : error
