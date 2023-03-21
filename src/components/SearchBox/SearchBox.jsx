0a827c6a8580606776dc91a55b360fb87b60f503
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-21 15:20:41 -0500

    searchbox

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState } from 'react'
+import { Button, Form, FormControl } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+
+const SearchBox = () => {
+    const [keyword, setKeyword] = useState('')
+    const navigate = useNavigate()
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+    }
+
+  return (
+    <Form onSubmit={submitHandler} inline>
+        <FormControl type='text' name='q' onChange={(e) => setKeyword(e.target.value)} className='mr-sm-2 ml-sm-5' >
+
+        </FormControl>
+        <Button type='submit' variant='outline-success' className='p-2'>
+            Submit
+        </Button>
+
+    </Form>
+  )
+}
+
+export default SearchBox
