4e1dabdd54413d304d6baa17d22b53391d1870d4
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-27 11:15:58 -0500

    commented actions

--- src/actions/cartActions.jsx
+++ src/actions/cartActions.jsx
@@ -1,3 +1,4 @@
+// Import the required packages
 import axios from "axios";
 import {
     CART_ADD_ITEM, 
@@ -6,9 +7,12 @@ import {
     CART_SAVE_PAYMENT_METHOD
 } from '../constants/cartConstants'
 
+// Define an asynchronous action creator for adding an item to the cart
 export const addToCart = (id, qty) => async (dispatch, getState) => {
+    // Fetch product data from the API using axios
     const {data} = await axios.get(`http://127.0.0.1:8000/api/products/${id}`)
 
+    // Dispatch the action to the reducer with the type and payload
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
@@ -21,34 +25,42 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
         }
     })
 
+    // Save the updated cart items to the local storage
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-
-
 }
 
+// Define an action creator for removing an item from the cart
 export const removeFromCart = (id) => (dispatch, getState) => {
+    // Dispatch the action to the reducer with the type and payload
     dispatch({
         type: CART_REMOVE_ITEM,
         payload: id,
     })
 
+    // Save the updated cart items to the local storage
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
 
+// Define an action creator for saving the shipping address
 export const saveShippingAddress = (data) => (dispatch) => {
+    // Dispatch the action to the reducer with the type and payload
     dispatch({
         type: CART_SAVE_SHIPPING_ADDRESS,
         payload: data,
     })
 
+    // Save the shipping address to the local storage
     localStorage.setItem('shippingAddress', JSON.stringify(data))
 }
 
+// Define an action creator for saving the payment method
 export const savePaymentMethod = (data) => (dispatch) => {
+    // Dispatch the action to the reducer with the type and payload
     dispatch({
         type: CART_SAVE_PAYMENT_METHOD,
         payload: data,
     })
 
+    // Save the payment method to the local storage
     localStorage.setItem('paymentMethod', JSON.stringify(data))
 }
